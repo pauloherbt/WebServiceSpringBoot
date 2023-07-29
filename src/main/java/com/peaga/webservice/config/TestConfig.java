6b62739fdcd101d40328809763bba661f50e9743
Peaga <pauloherbert@alu.ufc.br>
2023-07-29 15:24:47 -0300

    added layers service and repository and DB operations

---
+++ src/main/java/com/peaga/webservice/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.peaga.webservice.config;
+
+import com.peaga.webservice.entities.User;
+import com.peaga.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "");
+        userRepository.saveAll(List.of(user1,user2));
+    }
+}
